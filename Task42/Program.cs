23c6cb5654e121e1770eb1a9e4b614533916b815
Andrey Marymov <marymovandrey078115@gmail.com>
2022-09-05 22:52:45 +0300

    Task42

---
+++ Task42/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101 // 3  -> 11 // 2  -> 10
+/*
+Console.WriteLine("Введите десятичное число: ");
+int A = int.Parse(Console.ReadLine());
+
+string J = string.Empty;
+
+while (A > 0)
+{
+    J = A % 2 + J;
+    A = A / 2;
+}
+
+Console.WriteLine(J);
+*/
+Console.WriteLine("Введите десятичное число: ");
+int A = int.Parse(Console.ReadLine());
+int save = A;
+
+int count = 0;
+while (save > 0)
+{
+    save = save / 2;
+    count++;
+}
+
+int[] array = new int[count];
+int i = array.Length - 1;
+
+while (A > 0)
+{
+    array[i] = A % 2;
+    A = A / 2;
+    i--;
+}
+
+for (i = 0; i < array.Length; i++)
+{
+    Console.Write(array[i]);
+}
+Console.WriteLine();
