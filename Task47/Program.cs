5856098f7226deba4e5868fcf6c4d12bc4ebd987
Andrey Marymov <marymovandrey078115@gmail.com>
2022-09-08 23:05:47 +0300

    New Task

--- Task47/Program.cs
+++ Task47/Program.cs
@@ -1,6 +1,7 @@
 ﻿// Задача 47: Задайте двумерный массив размером m×n, 
 //заполненный случайными вещественными числами.
 
+/*
 Console.WriteLine("введите количество строк");
 int linesVol = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("введите количество столбцов");
@@ -33,3 +34,38 @@ void PrintArray(double[,] array)
         Console.WriteLine("");
     }
 }
+*/
+// Задача 47: Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+void FillArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Math.Round(new Random().Next(-10, 10) + new Random().NextDouble(), 1);
+        }
+    }
+}
+
+void Print(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Введите количество строк для двумерного массива: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов для двумерного массива: ");
+int n = int.Parse(Console.ReadLine());
+
+double[,] mass = new double[m, n];
+
+FillArray(mass);
+Print(mass);
---
+++ Task48/Program.cs
@@ -0,0 +1,49 @@
+﻿/*int[,] GetArray(int m, int n, int minValue, int maxValue) {
+  int[,] result = new int[m, n];
+
+  for (int i = 0; i < m; ++i) {
+    for (int j = 0; j < n; ++j) {
+      result[i, j] = new Random().Next(minValue, maxValue + 1);
+    }
+  }
+
+  return result;
+}
+
+void PrintArray(int[,] array) {
+  for (int i = 0; i < array.GetLength(0); ++i) {
+    for (int j = 0; j < array.GetLength(1); ++j) {
+      Console.Write(array[i, j]);
+    }
+    Console.WriteLine();
+  }
+}
+*/
+// Задача 48: Задайте двумерный массив размера m на n,
+// каждый элемент в массиве находится по формуле: Aₙₙ = m+n.
+// Выведите полученный массив на экран.
+
+void Print(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+int[,] mass = new int[4, 4];
+
+for (int i = 0; i < mass.GetLength(0); i++)
+{
+    for (int j = 0; j < mass.GetLength(1); j++)
+    {
+        mass[i, j] = i + j;
+    }
+}
+
+Print(mass);
